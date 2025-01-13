5d20ef024f0c172b2cae173741078b1561cf32e3
MaicolCode <paucarjhon07@outlook.com>
2025-01-13 10:29:20 -0500

    Se agrega funcion para obtener el estado actualizdo del post

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -1,21 +1,50 @@
 import { Router } from 'express'
 import { readFileSync } from 'fs'
+import { get } from 'http'
 import { resolve } from 'path'
 
 const indexRouter = Router()
 
-const posts = JSON.parse(readFileSync(resolve('./src/utils/posts.json')))
+const getPosts = () => {
+  return JSON.parse(readFileSync(resolve('./src/utils/posts.json')))
+}
+
+function getRecentPosts(posts, days) {
+  const now = new Date()
+  const limitDate = new Date()
+  limitDate.setDate(now.getDate() - days)
+
+  return posts.filter((post) => {
+    const postDate = new Date(post.date)
+    return postDate >= limitDate && postDate <= now
+  })
+}
 
 indexRouter.get('/', (req, res) => {
-  const recentPosts = posts.sort((a, b) => b.id - a.id).slice(0, 2)
+  const posts = getPosts()
 
-  res.render('index', { posts, recentPosts })
+  const recentPosts = getRecentPosts(posts, 2)
+  const orderedPosts = posts.sort((a, b) => b.id - a.id)
+
+  res.render('index', {
+    posts: orderedPosts,
+    recentPosts: recentPosts.sort((a, b) => b.id - a.id),
+    isAdmin: false
+  })
 })
 
 indexRouter.get('/post/:id', (req, res) => {
+  const posts = getPosts()
+
   const { id } = req.params
   const post = posts.find((post) => post.id == id)
-  res.render('viewPost', { post, posts: posts.filter((p) => p.id != id) })
+
+  if (!post) return res.status(404).render('404')
+  res.render('viewPost', {
+    post,
+    posts: posts.filter((p) => p.id != id),
+    isAdmin: false
+  })
 })
 
 export default indexRouter
