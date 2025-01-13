2565741b3dd5026637104d8b3988c2f33f8a1f55
MaicolCode <paucarjhon07@outlook.com>
2025-01-13 10:26:24 -0500

    Configuracion del editor de texto

---
+++ src/public/javascripts/my-tinymce-config.js
@@ -0,0 +1,9 @@
+tinymce.init({
+  selector: 'textarea#my-expressjs-tinymce-app',
+  plugins: ['image', 'fullscreen', 'lists'],
+  branding: false,
+  menubar: false,
+  toolbar:
+    'undo redo | styles forecolor| styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image | fullscreen | lists',
+  statusbar: false
+})
