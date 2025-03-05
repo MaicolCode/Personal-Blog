7259a4f7837feac20a295bc42a669bf016c0b6f0
MaicolCode <paucarjhon07@outlook.com>
2025-03-04 22:25:12 -0500

    Personalizacion de la ruta para permitir archivos estaticos de tinymce

--- src/middlewares/notFoundRoute.js
+++ src/middlewares/notFoundRoute.js
@@ -1,3 +1,6 @@
 export const notFoundRoute = (req, res) => {
+  if (req.path.startsWith('/tinymce')) {
+    return next() // Permite archivos estáticos de TinyMCE
+  }
   res.status(404).render('404')
 }
